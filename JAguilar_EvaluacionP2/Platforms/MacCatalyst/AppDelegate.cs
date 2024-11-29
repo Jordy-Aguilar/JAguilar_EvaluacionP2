9f0be10b30d365b051adadd9b97def05658260db
Jordy-Aguilar <jeay022@gmail.com>
2024-11-29 09:04:48 -0500

    Agregar archivos de proyecto.

---
+++ JAguilar_EvaluacionP2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JAguilar_EvaluacionP2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
